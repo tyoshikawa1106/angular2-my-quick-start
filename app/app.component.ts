aa0b4f2a5a1345eb4a5242ec84e4fa16f1060fa6
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-04-30 18:33:27 +0900

    オブジェクト変数Heroを追加

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,19 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   hero = 'Windstorm';
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
